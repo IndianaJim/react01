7c84cebe7cdf6f32063cbb256c23afb669b7d7bd
indianajim <jimrunkel@gmail.com>
2019-01-06 13:35:57 -0500

    finish basic test app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'reader'
+    };
+  }
+
+  handleNameChange = event => {
+    const value = event.target.value;
+    this.setState({
+      name: value
+    });
+  };
+
   render() {
     return (
       <div className="App">
-        <h1>Hello React World</h1>
+        <input
+          type={'text'}
+          placeholder={'Your name here'}
+          value={this.state.name}
+          onChange={this.handleNameChange}
+        />
+        <h1>Hello {this.state.name}!</h1>
       </div>
     );
   }
